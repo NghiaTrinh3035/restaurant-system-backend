9bbe42196cf867f9b05f7afaa7b4ec26418c8bee
Thành Đạt <thanhdat050625@gmail.com>
2026-08-06 09:56:03 +0700

    feat: forgot password api

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -4,12 +4,14 @@ import { RegisterDto } from '../dtos/register.dto';
 import { LoginDto } from '../dtos/login.dto';
 import { RequestRegisterOtpDto } from '../dtos/request-register-otp.dto';
 import { ResendOtpDto } from '../dtos/resend-otp.dto';
+import { ForgotPasswordDto } from '../dtos/forgot-password.dto';
+import { ResetPasswordDto } from '../dtos/reset-password.dto';
 import { Public } from 'src/core/common/decorators/public.decorator';
 import { ResponseMessage } from 'src/core/common/decorators/response-message.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Public()
   @ResponseMessage('OTP đã được gửi thành công')
@@ -17,14 +19,6 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   async requestOtp(@Body() body: RequestRegisterOtpDto) {
     return this.authService.requestOtp(body);
-  }
-
-  @Public()
-  @ResponseMessage('OTP mới đã được gửi thành công')
-  @Post('register/resend-otp')
-  @HttpCode(HttpStatus.OK)
-  async resendOtp(@Body() body: ResendOtpDto) {
-    return this.authService.resendOtp(body);
   }
 
   @Public()
@@ -41,5 +35,29 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   async login(@Body() body: LoginDto) {
     return this.authService.login(body);
+  }
+
+  @Public()
+  @ResponseMessage('Yêu cầu đặt lại mật khẩu thành công')
+  @Post('forgot-password')
+  @HttpCode(HttpStatus.OK)
+  async forgotPassword(@Body() body: ForgotPasswordDto) {
+    return this.authService.forgotPassword(body);
+  }
+
+  @Public()
+  @ResponseMessage('Đặt lại mật khẩu thành công')
+  @Post('reset-password')
+  @HttpCode(HttpStatus.OK)
+  async resetPassword(@Body() body: ResetPasswordDto) {
+    return this.authService.resetPassword(body);
+  }
+
+  @Public()
+  @ResponseMessage('OTP mới đã được gửi thành công')
+  @Post('resend-otp')
+  @HttpCode(HttpStatus.OK)
+  async resendOtp(@Body() body: ResendOtpDto) {
+    return this.authService.resendOtp(body);
   }
 }
