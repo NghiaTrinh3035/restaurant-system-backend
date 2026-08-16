cc7c1c91b2a6ae55b4c054aaceb34089c84aff86
NghiaTrinh3035 <nghiatrinh3035@gmail.com>
2026-08-16 12:30:08 +0700

    feat: triển khai lấy role từ token & triển khai các api lấy danh sách, lấy chi tiết và tạo chi nhánh nhà hàng

---
+++ src/modules/restaurants/controllers/restaurant-branch.controller.ts
@@ -0,0 +1,62 @@
+import {
+    Body,
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Param,
+    Post,
+    UseGuards,
+} from '@nestjs/common';
+
+import { RestaurantBranchService } from '../services/restaurant-branch.service';
+
+import { ResponseMessage } from 'src/core/common/decorators/response-message.decorator';
+
+import { Role } from '@prisma/client';
+
+import { JwtAuthGuard } from 'src/core/common/guards/jwt-auth.guard';
+
+import { CreateRestaurantBranchDto } from '../dto/create-restaurant-branch.dto';
+import { Public } from 'src/core/common/decorators/public.decorator';
+import { Roles } from 'src/core/common/decorators/roles.decorator';
+
+@Controller('/restaurants/branches')
+export class RestaurantBranchController {
+
+    constructor(
+        private readonly restaurantBranchService: RestaurantBranchService,
+    ) { }
+
+    // PUBLIC
+    @Get()
+    @Public()
+    @HttpCode(HttpStatus.OK)
+    @ResponseMessage('Lấy danh sách chi nhánh nhà hàng thành công.')
+    async getRestaurantBranches() {
+        return this.restaurantBranchService.getRestaurantBranches();
+    }
+
+    // PUBLIC
+    @Get('/:id')
+    @Public()
+    @HttpCode(HttpStatus.OK)
+    @ResponseMessage('Lấy thông tin chi nhánh nhà hàng thành công.')
+    async getRestaurantBranch(
+        @Param('id') id: string,
+    ) {
+        return this.restaurantBranchService.getRestaurantBranch(id);
+    }
+
+    // ADMIN ONLY
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    @UseGuards(JwtAuthGuard)
+    @Roles(Role.ADMIN)
+    @ResponseMessage('Thêm chi nhánh nhà hàng thành công.')
+    async createRestaurantBranch(
+        @Body() dto: CreateRestaurantBranchDto,
+    ) {
+        return this.restaurantBranchService.createRestaurantBranch(dto);
+    }
+}
--- src/modules/restaurants/restaurants.module.ts
+++ src/modules/restaurants/restaurants.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RestaurantsController } from './controllers/restaurants.controller';
 import { RestaurantsService } from './services/restaurants.service';
+import { RestaurantBranchController } from './controllers/restaurant-branch.controller';
+import { RestaurantBranchService } from './services/restaurant-branch.service';
 
 @Module({
-  controllers: [RestaurantsController],
-  providers: [RestaurantsService],
+  controllers: [RestaurantsController, RestaurantBranchController],
+  providers: [RestaurantsService, RestaurantBranchService],
 })
-export class RestaurantsModule {}
+export class RestaurantsModule { }
