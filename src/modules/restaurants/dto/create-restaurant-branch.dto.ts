5ecfaa5b746000f65f6cac1dce90d2fe8fe08004
NghiaTrinh3035 <nghiatrinh3035@gmail.com>
2026-08-30 21:59:58 +0700

    fix: use new logic address in restaurant-branch

--- src/modules/restaurants/dto/create-restaurant-branch.dto.ts
+++ src/modules/restaurants/dto/create-restaurant-branch.dto.ts
@@ -1,13 +1,25 @@
-import { IsEnum, IsNumber, IsString } from "class-validator";
+import { IsEnum, IsNumber, IsString, ValidateNested } from "class-validator";
+import { Type } from 'class-transformer';
 import { RestaurantBranchStatus } from '@prisma/client';
 
+class CreateRestaurantBranchAddressDto {
+    @IsString()
+    provinceCode: string;
+
+    @IsString()
+    wardCode: string;
+
+    @IsString()
+    detail: string;
+}
 
 export class CreateRestaurantBranchDto {
     @IsString()
     name: string;
 
-    @IsString()
-    address: string;
+    @ValidateNested()
+    @Type(() => CreateRestaurantBranchAddressDto)
+    address: CreateRestaurantBranchAddressDto;
 
     @IsString()
     phone: string;
